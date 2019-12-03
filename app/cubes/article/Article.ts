2799bf0bc9ab7258f7d51e16330a57daf9bdec85
ssaraikin <ssaraikin@gmail.com>
2019-12-03 23:22:46 +0700

    Remove unnecessary cast from entity

--- app/cubes/article/Article.ts
+++ app/cubes/article/Article.ts
@@ -2,24 +2,20 @@ import { AbstractEntity } from '../../../core/entity/AbstractEntity'
 import * as yup from 'yup'
 import { AbstractObject } from '../../../core/AbstractObject'
 
-interface IArticleData extends AbstractObject {
-  title: string
-  author: string
-}
+const scheme = yup.object().shape({
+  id: yup.number(),
+  title: yup.string(),
+  author: yup.string(),
+})
 
-export class Article extends AbstractEntity<IArticleData> implements IArticleData {
+type TArticle = yup.InferType<typeof scheme>
+
+export class Article extends AbstractEntity<TArticle> implements TArticle {
   public title = ''
   public author = ''
 
-  public static cast(data: AbstractObject): IArticleData {
-    return yup
-      .object()
-      .shape({
-        id: yup.string(),
-        title: yup.string(),
-        author: yup.string(),
-      })
-      .noUnknown()
+  public static cast(data: AbstractObject): TArticle {
+    return scheme.noUnknown()
       .cast(data)
   }
 }
