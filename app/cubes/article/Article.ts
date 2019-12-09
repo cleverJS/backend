54d2121dbca6811a3458c2193b802cd340e23d4a
ssaraikin <ssaraikin@gmail.com>
2019-12-09 10:35:56 +0700

    test

--- app/cubes/article/Article.ts
+++ app/cubes/article/Article.ts
@@ -6,6 +6,7 @@ const scheme = yup.object().shape({
   id: yup.number(),
   title: yup.string(),
   author: yup.string(),
+  content: yup.string(),
 })
 
 type TArticle = yup.InferType<typeof scheme>
@@ -13,9 +14,9 @@ type TArticle = yup.InferType<typeof scheme>
 export class Article extends AbstractEntity<TArticle> implements TArticle {
   public title = ''
   public author = ''
+  public content = ''
 
   public static cast(data: AbstractObject): TArticle {
-    return scheme.noUnknown()
-      .cast(data)
+    return scheme.noUnknown().cast(data)
   }
 }
