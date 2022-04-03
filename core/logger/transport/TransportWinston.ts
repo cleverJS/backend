5e2ab0fd52503333192c38c88406862b618ae5ef
ssaraykin <ssaraikin@gmail.com>
2022-04-03 19:15:42 +0200

    - clean

--- core/logger/transport/TransportWinston.ts
+++ core/logger/transport/TransportWinston.ts
@@ -1,12 +1,10 @@
-import winston from 'winston'
+import { format, Logger, createLogger, transports } from 'winston'
 import fs from 'fs'
 import { LogLevel, TransportInterface } from './TransportInterface'
 import { argsStringify } from '../../utils/common'
 
-const { format } = winston
-
 export class TransportWinston implements TransportInterface {
-  protected readonly logger: winston.Logger
+  protected readonly logger: Logger
 
   public constructor(logDir: string) {
     if (!fs.existsSync(logDir)) {
@@ -19,15 +17,15 @@ export class TransportWinston implements TransportInterface {
 
     const appDebug = (process.env.APP_DEBUG || 'false') === 'true'
 
-    this.logger = winston.createLogger({
+    this.logger = createLogger({
       level: process.env.NODE_ENV === 'production' && !appDebug ? 'info' : 'debug',
-      format: winston.format.json(),
+      format: format.json(),
       transports: [
         //
         // - Write all logs with level `error` and below to `error.log`
         // - Write all logs with level `info` and below to `combined.log`
         //
-        new winston.transports.File({
+        new transports.File({
           filename: `${logDir}/error.log`,
           handleExceptions: true,
           level: 'error',
@@ -35,14 +33,14 @@ export class TransportWinston implements TransportInterface {
           maxFiles: 5,
           format: format.combine(format.timestamp(), format.prettyPrint(), logFormat),
         }),
-        new winston.transports.File({
+        new transports.File({
           filename: `${logDir}/combined.log`,
           handleExceptions: true,
           maxsize: 5242880, // 5MB
           maxFiles: 5,
           format: format.combine(format.timestamp(), format.prettyPrint(), logFormat),
         }),
-        new winston.transports.Console({
+        new transports.Console({
           handleExceptions: true,
           format: format.combine(format.timestamp(), format.prettyPrint(), format.colorize(), logFormat),
         }),
