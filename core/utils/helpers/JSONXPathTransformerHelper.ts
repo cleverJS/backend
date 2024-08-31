3cc582ebad2e73546de836143ab8296027f3b7fd
ssaraykin <ssaraikin@gmail.com>
2024-08-31 12:38:50 +0200

    - package.json update - Add JSON XPath Transformer

---
+++ core/utils/helpers/JSONXPathTransformerHelper.ts
@@ -0,0 +1,141 @@
+const parseRegex = /^parse\((.+)\)(\/.+)?$/
+
+export type JSONXpathTransformConfig = {
+  [sourcePath: string]: string
+}
+
+export class JSONXPathTransformerHelper {
+  /**
+   * Transform json object from one structure to another
+   *
+   * @example
+   *     const config: JSONXpathTransformConfig = {
+   *       'user/name': 'fullName',
+   *       'user/age': 'age',
+   *       'user/address/street': 'location/street',
+   *       'user/address/city': 'location/city',
+   *       'job/title': 'occupation',
+   *       'job/company': 'employer',
+   *       'stringify(user/address/street,user/address/city,parse(preferences/settings)/country:user/address/country)': 'locationJson',
+   *       'parse(preferences/settings)': 'settings',
+   *       'parse(preferences/history)': 'visitedPages',
+   *     }
+   *
+   * @param {Record<string, any>} input
+   * @param {JSONXpathTransformConfig} config
+   */
+  static transform(input: Record<string, any>, config: JSONXpathTransformConfig): any {
+    const output: Record<string, any> = {}
+    for (const [source, target] of Object.entries(config)) {
+      let value
+      if (source.startsWith('stringify(')) {
+        value = this.#stringify(input, source)
+      } else if (source.startsWith('parse(')) {
+        value = this.#parse(input, source)
+      } else {
+        value = this.#getValueByPath(input, source)
+      }
+
+      if (value !== undefined) {
+        this.#setValueByPath(output, target, value)
+      }
+    }
+
+    return output
+  }
+
+  static #stringifyKeysToConfig(keys: string[]) {
+    return keys.reduce(
+      (acc, item) => {
+        const [key, value] = item.split(':')
+
+        let nextValue: string | undefined = value
+
+        if (!nextValue) {
+          if (key.startsWith('parse(')) {
+            const match = key.match(parseRegex)
+
+            if (!match?.[1]) {
+              throw new Error(`keys for parsing were not matched: ${key}`)
+            }
+
+            if (match?.[2]) {
+              nextValue = match[2].slice(1)
+            } else {
+              nextValue = match[1].split('/').pop()
+            }
+          } else {
+            nextValue = key
+          }
+        }
+
+        if (!nextValue) {
+          throw new Error('Cannot assemble keys to config')
+        }
+
+        acc[key] = nextValue
+        return acc
+      },
+      {} as Record<string, any>
+    )
+  }
+
+  static #getValueByPath(obj: any, path: string): any {
+    const keys = path.split('/')
+    let result = obj
+    for (const key of keys) {
+      if (key in result) {
+        result = result[key]
+      } else {
+        return undefined
+      }
+    }
+    return result
+  }
+
+  static #setValueByPath(obj: any, path: string, value: any): void {
+    const keys = path.split('/')
+    let current = obj
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i]
+      if (i === keys.length - 1) {
+        current[key] = value
+      } else {
+        if (!current[key]) {
+          current[key] = {}
+        }
+        current = current[key]
+      }
+    }
+  }
+
+  static #parse(input: any, source: string) {
+    const match = source.match(parseRegex)
+    if (!match?.[1]) {
+      throw new Error(`keys for parsing were not matched: ${source}`)
+    }
+
+    const key = match[1]
+    let value = this.#getValueByPath(input, key)
+    value = JSON.parse(value)
+
+    if (match?.[2]) {
+      value = this.#getValueByPath(value, match?.[2].slice(1))
+    }
+
+    return value
+  }
+
+  static #stringify(input: any, source: string): string {
+    const match = source.match(/stringify\((.*)\)/)
+
+    if (!match?.[1]) {
+      throw new Error(`keys for stringify were not matched: ${source}`)
+    }
+
+    const keys = match[1].split(',')
+    const config = this.#stringifyKeysToConfig(keys)
+    const output = JSONXPathTransformerHelper.transform(input, config)
+    return JSON.stringify(output)
+  }
+}
---
+++ tests/utils/helpers/JSONXPathTransformer.spec.ts
@@ -0,0 +1,109 @@
+import { logger } from '../../../core/logger/logger'
+import { JSONXpathTransformConfig, JSONXPathTransformerHelper } from '../../../core/utils/helpers/JSONXPathTransformerHelper'
+
+describe('Test JSONXPathTransformerHelper', () => {
+  it('should stringify target node', async () => {
+    const config: JSONXpathTransformConfig = {
+      'stringify(user/address/street:street,user/address/city:city)': 'location',
+    }
+
+    try {
+      const result = JSONXPathTransformerHelper.transform(input, config)
+
+      expect({
+        location: '{"street":"123 Main St","city":"Anytown"}',
+      }).toEqual(result)
+    } catch (e) {
+      logger.error(e)
+    }
+  })
+
+  it('should stringify target node with a parsed value', async () => {
+    const config: JSONXpathTransformConfig = {
+      'stringify(user/address/street:street,user/address/city:city,parse(preferences/settings)/country:country)': 'location',
+    }
+
+    try {
+      const result = JSONXPathTransformerHelper.transform(input, config)
+
+      expect({
+        location: '{"street":"123 Main St","city":"Anytown","country":"Slovenia"}',
+      }).toEqual(result)
+    } catch (e) {
+      logger.error(e)
+    }
+  })
+
+  it('should stringify target node with a parsed values', async () => {
+    const config: JSONXpathTransformConfig = {
+      'stringify(user/address/street:street,user/address/city:city,parse(preferences/settings):settings)': 'location',
+    }
+
+    try {
+      const result = JSONXPathTransformerHelper.transform(input, config)
+
+      expect({
+        location: `{\"street\":\"123 Main St\",\"city\":\"Anytown\",\"settings\":{\"theme\":\"dark\",\"notifications\":true,\"country\":\"Slovenia\"}}`,
+      }).toEqual(result)
+    } catch (e) {
+      logger.error(e)
+    }
+  })
+
+  it('should transform', async () => {
+    const config: JSONXpathTransformConfig = {
+      'user/name': 'fullName',
+      'user/age': 'age',
+      'user/address/street': 'location/street',
+      'user/address/city': 'location/city',
+      'job/title': 'occupation',
+      'job/company': 'employer',
+      'stringify(user/address/street,user/address/city,parse(preferences/settings)/country:user/address/country)': 'locationJson',
+      'parse(preferences/settings)': 'settings',
+      'parse(preferences/history)': 'visitedPages',
+    }
+
+    try {
+      const result = JSONXPathTransformerHelper.transform(input, config)
+
+      expect({
+        fullName: 'John Doe',
+        age: 30,
+        location: {
+          street: '123 Main St',
+          city: 'Anytown',
+        },
+        occupation: 'Developer',
+        employer: 'Tech Corp',
+        locationJson: `{\"user\":{\"address\":{\"street\":\"123 Main St\",\"city\":\"Anytown\",\"country\":\"Slovenia\"}}}`,
+        settings: {
+          theme: 'dark',
+          notifications: true,
+          country: 'Slovenia',
+        },
+        visitedPages: ['page1', 'page2'],
+      }).toEqual(result)
+    } catch (e) {
+      logger.error(e)
+    }
+  })
+})
+
+const input = {
+  user: {
+    name: 'John Doe',
+    age: 30,
+    address: {
+      street: '123 Main St',
+      city: 'Anytown',
+    },
+  },
+  job: {
+    title: 'Developer',
+    company: 'Tech Corp',
+  },
+  preferences: {
+    settings: '{"theme":"dark","notifications":true,"country":"Slovenia"}',
+    history: '["page1","page2"]',
+  },
+}
